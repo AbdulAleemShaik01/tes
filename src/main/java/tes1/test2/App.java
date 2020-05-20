944cb852d919b8a75d8d166571c5bde714f8691d
AbdulAleemShaik01 <abdulaleem.3548@gmail.com>
2020-05-20 15:57:02 +0530

    third commit

---
+++ src/main/java/tes1/test2/App.java
@@ -0,0 +1,13 @@
+package tes1.test2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
